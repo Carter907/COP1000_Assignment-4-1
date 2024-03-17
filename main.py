7eeb569aa42656143d8fde29bad769fb2781b424
carter <speers.carter@gmail.com>
2024-03-17 11:40:06 -0400

    first commit

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ This program calculates prices for custom house signs.
 """
 
 # Declare and initialize variables here.
+
 # Charge for this sign.
+charge = 0.00
 # Number of characters.
+numChars = 8
 # Color of characters.
+color = 'gold'
 # Type of wood.
+woodType = 'oak'
+
+charge = 35
+if numChars > 5:
+    charge += (numChars - 5)*4
+if woodType == 'oak':
+    charge += 20
+if color == 'gold':
+    charge += 15
 
 # Write assignment and if statements here as appropriate.
 
